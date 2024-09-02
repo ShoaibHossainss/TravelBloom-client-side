77a3f6887cb54385046f9569e8b3b25687ffe66f
Shovanss <imshovanss@gmail.com>
2024-09-02 19:03:56 +0600

    tour type section added

--- src/Pages/Tourtype/TourType.jsx
+++ src/Pages/Tourtype/TourType.jsx
@@ -1,4 +1,10 @@
 import { Link } from "react-router-dom";
+import image1 from '../../assets/Firefly adventure 73216.jpg'
+import image2 from '../../assets/Firefly Historical Tour of Ancient Rome 7759.jpg'
+import image3 from '../../assets/Firefly Luxury Spa Retreat in Bali- 67717.jpg'
+import image4 from '../../assets/Firefly African Safari Adventure 66097.jpg'
+import image5 from '../../assets/Firefly Mediterranean Cruise Experience 99504.jpg'
+import image6 from '../../assets/Firefly Amazon Rainforest Expedition 39528.jpg'
 
 
 const TourType = () => {
@@ -6,25 +12,61 @@ const TourType = () => {
    
    
     return (
-        <div className="flex gap-6 mx-auto justify-center">
-            <Link to='/adventure'>
-            <div className="text-center border-[5px]">Adventure</div>
-            </Link>
-            <Link to='/cultural'>
-            <div className="text-center border-[5px]">Cultural</div>
-            </Link>
-            <Link to='/relaxation'>
-            <div className="text-center border-[5px]">Relaxation</div>
-            </Link>
-            <Link to='/wildlife'>
-            <div className="text-center border-[5px]">Wildlife</div>
-            </Link>
-            <Link to='/cruise'>
-            <div className="text-center border-[5px]">Cruise</div>
-            </Link>
-            <Link to='/eco-tourism'>
-            <div className="text-center border-[5px]">Eco-Tourism</div>
-            </Link>
+        <div>
+            <h3 className="text-center">Look at our packages and
+                <br />
+                <span>browse your favorite ones</span>
+            </h3>
+            <div className="grid grid-cols-3 gap-6 mx-auto p-4">
+            <div className="text-center">
+                <img className="opacity-90"  src={image1} alt="" />
+               <Link to='/adventure'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+                Adventure
+                </button>
+                </Link>
+                </div>
+            <div className="text-center">
+                <img className="opacity-90"  src={image2} alt="" />
+                <Link to='/cultural'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+               Cultural
+                </button>
+                </Link>
+                </div>
+            <div className="text-center">
+                <img className="opacity-90"  src={image3} alt="" />
+                <Link to='/relaxation'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+               Relaxation
+                </button>
+                </Link>
+                </div>
+            <div className="text-center">
+                <img className="opacity-90"  src={image4} alt="" />
+                <Link to='/wildlife'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+               Wildlife
+                </button>
+                </Link>
+                </div>
+            <div className="text-center">
+                <img className="opacity-90"  src={image5} alt="" />
+                <Link to='/cruise'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+               Cruise
+                </button>
+                </Link>
+                </div>
+            <div className="text-center">
+                <img className="opacity-90"  src={image6} alt="" />
+                <Link to='/eco-tourism'>
+               <button className="btn px-4 pb-1 bg-green-500 -translate-y-12 text-2xl border-none">
+               Eco-Tourism
+                </button>
+                </Link>
+                </div>
+        </div>
         </div>
     );
 };
